200550f3922f77bcc6e64a04c08b7124cb9a30a2
kosovic-creator <drasko.kosovic@icloud.com>
2025-03-29 07:51:43 +0100

    init

---
+++ src/lib/db/migrations/20250329062816_inits/migration.sql
@@ -0,0 +1,32 @@
+/*
+  Warnings:
+
+  - The primary key for the `Account` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - The primary key for the `VerificationToken` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - A unique constraint covering the columns `[provider,providerAccountId]` on the table `Account` will be added. If there are existing duplicate values, this will fail.
+  - A unique constraint covering the columns `[identifier,token]` on the table `VerificationToken` will be added. If there are existing duplicate values, this will fail.
+  - The required column `id` was added to the `Account` table with a prisma-level default value. This is not possible if the table is not empty. Please add this column as optional, then populate it before making it required.
+  - The required column `id` was added to the `Session` table with a prisma-level default value. This is not possible if the table is not empty. Please add this column as optional, then populate it before making it required.
+
+*/
+-- AlterTable
+ALTER TABLE "Account" DROP CONSTRAINT "Account_pkey",
+ADD COLUMN     "id" TEXT NOT NULL,
+ADD CONSTRAINT "Account_pkey" PRIMARY KEY ("id");
+
+-- AlterTable
+ALTER TABLE "Session" ADD COLUMN     "id" TEXT NOT NULL,
+ADD CONSTRAINT "Session_pkey" PRIMARY KEY ("id");
+
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "password" TEXT,
+ALTER COLUMN "email" DROP NOT NULL;
+
+-- AlterTable
+ALTER TABLE "VerificationToken" DROP CONSTRAINT "VerificationToken_pkey";
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Account_provider_providerAccountId_key" ON "Account"("provider", "providerAccountId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "VerificationToken_identifier_token_key" ON "VerificationToken"("identifier", "token");
