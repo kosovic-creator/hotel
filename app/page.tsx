c060a8c5bb2e1d97918e50f5633f2d52024a49f8
kosovic-creator <drasko.kosovic@icloud.com>
2025-05-22 15:56:29 +0200

    init

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+import RezervacijaForma from "@/components/RezervacijaForma";
 import "./globals.css";
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
       <div className="w-full max-w-lg">
-        {/* <RezervacijaForma apartmanId={7} /> */}
+        <RezervacijaForma apartmanId={7} />
       </div>
     </div>
   );
